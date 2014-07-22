76836389ee093e4a2d25483cc10bea821a8e5f21
winifred <wcadap@gmail.com>
2014-07-22 15:48:48 +0800

    First and Last commit

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SingletonDesignPattern.Singleton;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileLogger = FileLogger.Instance;
+
+            fileLogger.WriteToFile("Singleton is awesome for Performance.");
+            fileLogger.WriteToFile("More from Singleton..");
+
+            //Test for to see if another instance will be created. 
+            //Singleton Initialized should be printed twice.
+            var fileLogger1 = FileLogger.Instance;
+
+            fileLogger1.WriteToFile("Using another instance...");
+            fileLogger1.WriteToFile("More from another instance.");
+
+            fileLogger.CloseFile();
+            fileLogger1.CloseFile();
+
+            Console.ReadKey();
+        }
+    }
+
+        
+}
---
+++ SingletonDesignPattern/Singleton/FileLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace SingletonDesignPattern.Singleton
+{
+    class FileLogger
+    {
+        private static FileLogger _instance;
+        private static System.IO.StreamWriter TextFile = new System.IO.StreamWriter(Directory.GetCurrentDirectory() + "\\Log.txt", true);
+        private static object lockThis = new object();
+        
+        private FileLogger() { }
+        public static FileLogger Instance
+        {
+            get
+            {
+                lock (lockThis) { 
+                    Console.WriteLine("Singleton invoked.");
+                    if (_instance == null)
+                    {
+                        _instance = new FileLogger();
+                        Console.WriteLine("Singleton Initialized.");
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public void WriteToFile(string text)
+        {
+            Console.WriteLine("Writing to File...");
+            TextFile.WriteLine(text);
+        }
+
+        public void CloseFile()
+        {
+            TextFile.Close();
+            TextFile.Dispose();
+        }
+    
+    }
+}
